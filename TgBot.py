7ff0d8c9dd5bde54fec737174250d6541938a957
MeCAShBack <hunql454@gmail.com>
2026-07-23 17:59:33 +0600

    Implement user registration and database functionality
    Added SQLite database support to store user information. Implemented user registration and a command to display registered users.

--- TgBot.py
+++ TgBot.py
@@ -2,11 +2,59 @@ import telebot
 import requests
 from telebot import types
 from currency_converter import CurrencyConverter
+import sqlite3
 bot = telebot.TeleBot("8785518185:AAFntd8GrcqS8oUmZaBL-pX35U-5KvY1h5I")
 API = "7a9ddc019331b2a2bf9020cd4eb5f777"
 coin = CurrencyConverter()
+def create_db():
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY,
+            username TEXT,
+            first_name TEXT,
+            date TEXT
+        )
+    """)
+    conn.commit()
+    conn.close()
+
+def save_user(user_id, username, first_name):
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+    cursor.execute("""
+        INSERT OR IGNORE INTO users (id, username, first_name, date)
+        VALUES (?, ?, ?, datetime('now'))
+    """, (user_id, username, first_name))
+    conn.commit()
+    conn.close()
+create_db()
+@bot.message_handler(commands=["users"])
+def show_users(message):
+    MY_ID = 1994959702
+    if message.from_user.id != MY_ID:
+        bot.send_message(message.chat.id, "❌ У тебя нет доступа!")
+        return
+
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users")
+    users = cursor.fetchall()
+    conn.close()
+
+    text = f"👥 Всего пользователей: {len(users)}\n\n"
+    for user in users:
+        text += f"👤 {user[2]} (@{user[1]})\n"
+
+    bot.send_message(message.chat.id, text)
 @bot.message_handler(commands=["start"])
 def send_start(message):
+    save_user(
+        message.from_user.id,
+        message.from_user.username,
+        message.from_user.first_name
+    )
     markup = types.ReplyKeyboardMarkup()
     btn1 = types.KeyboardButton("my creator")
     markup.row(btn1)
@@ -74,24 +122,30 @@ def currency(message):
     bot.register_next_step_handler(message, get_amount)
 
 def get_amount(message):
+    @bot.message_handler(commands=["currency"])
+def currency(message):
+    bot.send_message(message.chat.id , "Введите число:" , parse_mode="HTML")
+    bot.register_next_step_handler(message, get_amount)
+
+def get_amount(message):
     try:
-        amount = float(message.text.strip())
+        amount = float(message.text.strip().lower())
     except ValueError:
         bot.reply_to(message, "❌ Введите число!")
         bot.register_next_step_handler(message, get_amount)
         return
-    if amount > 0:
-        markup = types.InlineKeyboardMarkup()
-        btn1 = types.InlineKeyboardButton("USD → EUR", callback_data=f"USD_EUR_{amount}")
-        btn2 = types.InlineKeyboardButton("EUR → USD", callback_data=f"EUR_USD_{amount}")
-        btn3 = types.InlineKeyboardButton("USD → KGS", callback_data=f"USD_KGS_{amount}")
-        btn4 = types.InlineKeyboardButton("EUR → KGS" , callback_data=f"EUR_KSG_{amount}")
-        markup.row(btn1, btn2)
-        markup.row(btn3, btn4)
-        bot.send_message(message.chat.id, "Выберите валюту:", reply_markup=markup)
-    else:
-        bot.send_message(message.chat.id , "число должен быть больше 0.впешите сумму")
-        bot.register_next_step_handler(message, get_amount)
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton("USD → EUR", callback_data=f"USD_EUR_{amount}")
+    btn2 = types.InlineKeyboardButton("EUR → USD", callback_data=f"EUR_USD_{amount}")
+    btn3 = types.InlineKeyboardButton("USD → KGS", callback_data=f"USD_KGS_{amount}")
+    btn4 = types.InlineKeyboardButton("EUR → KGS" ,callback_data=f"EUR_KGS_{amount}")
+    markup.row(btn1, btn2)
+    markup.row(btn3, btn4)
+    file = open("currency_money .jpg", "rb")
+    bot.send_photo(
+        message.chat.id, file
+    )
+    bot.send_message(message.chat.id, "Выберите валюту:", reply_markup=markup)
 @bot.callback_query_handler(func=lambda call: "_" in call.data)
 def convert_currency(call):
     parts = call.data.split("_")
@@ -99,13 +153,17 @@ def convert_currency(call):
     to_currency = parts[1]
     amount = float(parts[2])
     try:
-        result = coin.convert(amount,from_currency,to_currency)
+        res = requests.get(f"https://api.exchangerate-api.com/v4/latest/{from_currency}")
+        data = res.json()
+        rate = data["rates"][to_currency]
+        result = amount * rate
         bot.send_message(
             call.message.chat.id,
             f"{amount} {from_currency} = {result:.2f} {to_currency}"
         )
-    except:
-        bot.send_message(call.message.chat.id, "❌ Ошибка конвертации")
+    except KeyError:
+        bot.send_message(call.message.chat.id,"❌ Ошибка конвертации")
+    
 
 @bot.message_handler(commands=["help"])
 def get_help(message):
